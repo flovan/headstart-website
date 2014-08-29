8099bff376ced88f25956e13d2436a14c4a95d8b
Florian <winteregel@flovan.local>
2014-08-29 22:42:15 +0200

    Adding roadmap to have copy (deleting again next)

---
+++ app/views/roadmap.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.main')
+
+@section('content')
+<div class="wrapper wrapper--intro">
+	<h1>Roadmap</h1>
+</div>
+<div class="wrapper wrapper--content">
+    <p>Headstart is going to continue evolving in order to stay easy and modern. To achieve this, features will be get added and removed when it makes sense to do so. And even though this project revolves around the idea of having a zero-configuration workflow for <em>everyone</em>, it is still kind of opinionated.</p>
+    <p>The list below contains features or ideas that might find their way into Headstart. </p>
+</div>
+@stop
